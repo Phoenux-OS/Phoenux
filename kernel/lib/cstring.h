63a003f077656c0ea6f803826ee1f2f2bc140ec0
mintsuki <mintsuki@protonmail.com>
2019-10-09 11:54:28 +0200

    add cstring

---
+++ kernel/lib/cstring.h
@@ -0,0 +1,12 @@
+#ifndef __LIB__CSTRING_H__
+#define __LIB__CSTRING_H__
+
+#include <stddef.h>
+
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+size_t strlen(const char *str);
+
+#endif
