5f41c202ac2c27c80cbf8d060f5501cdc3947f94
mintsuki <mintsuki@protonmail.com>
2019-10-09 12:32:31 +0200

    improve cmem

--- kernel/lib/cmem.c
+++ kernel/lib/cmem.c
@@ -2,41 +2,38 @@
 #include <stdint.h>
 #include <lib/cmem.h>
 
-void *memcpy(void *dest, const void *src, size_t count) {
-    size_t i = 0;
+void *memcpy(void *dest, const void *src, size_t n) {
+    uint8_t *pdest = dest;
+    const uint8_t *psrc = src;
 
-    uint8_t *dest2 = dest;
-    const uint8_t *src2 = src;
-
-    for (i = 0; i < count; i++) {
-        dest2[i] = src2[i];
+    for (size_t i = 0; i < n; i++) {
+        pdest[i] = psrc[i];
     }
 
     return dest;
 }
 
-void *memset(void *s, int c, size_t count) {
-    uint8_t *p = s, *end = p + count;
-    for (; p != end; p++) {
-        *p = (uint8_t)c;
+void *memset(void *s, int c, size_t n) {
+    uint8_t *p = s;
+
+    for (size_t i = 0; i < n; i++) {
+        p[i] = (uint8_t)c;
     }
 
     return s;
 }
 
-void *memmove(void *dest, const void *src, size_t count) {
-    size_t i = 0;
-
-    uint8_t *dest2 = dest;
-    const uint8_t *src2 = src;
+void *memmove(void *dest, const void *src, size_t n) {
+    uint8_t *pdest = dest;
+    const uint8_t *psrc = src;
 
     if (src > dest) {
-        for (i = 0; i < count; i++) {
-            dest2[i] = src2[i];
+        for (size_t i = 0; i < n; i++) {
+            pdest[i] = psrc[i];
         }
     } else if (src < dest) {
-        for (i = count; i > 0; i--) {
-            dest2[i - 1] = src2[i - 1];
+        for (size_t i = n; i > 0; i--) {
+            pdest[i-1] = psrc[i-1];
         }
     }
 
@@ -44,15 +41,12 @@ void *memmove(void *dest, const void *src, size_t count) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-    const uint8_t *a = s1;
-    const uint8_t *b = s2;
+    const uint8_t *p1 = s1;
+    const uint8_t *p2 = s2;
 
     for (size_t i = 0; i < n; i++) {
-        if (a[i] < b[i]) {
-            return -1;
-        } else if (a[i] > b[i]) {
-            return 1;
-        }
+        if (p1[i] != p2[i])
+            return p1[i] < p2[i] ? -1 : 1;
     }
 
     return 0;
--- kernel/lib/cmem.h
+++ kernel/lib/cmem.h
@@ -3,9 +3,9 @@
 
 #include <stddef.h>
 
-void *memcpy(void *dest, const void *src, size_t count);
-void *memset(void *s, int c, size_t count);
-void *memmove(void *dest, const void *src, size_t count);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
